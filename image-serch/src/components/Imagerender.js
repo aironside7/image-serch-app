3b6b45ff48a92a4c14471e67942011b5041fe70c
aironside7 <aniroycr7@gmail.com>
2022-12-12 20:07:42 +0530

    updated

--- image-serch/src/components/Imagerender.js
+++ image-serch/src/components/Imagerender.js
@@ -1,17 +1,20 @@
 import React from 'react'
 
-const Imagerender = () => {
-    // constructor()
-  return (
-    <>
-        <div className='imageren' > 
-            <img src='https://images.unsplash.com/photo-1669617450318-318cc22b9cab?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=387&q=80' alt=''/>
-            <img src='https://images.unsplash.com/photo-1669617450318-318cc22b9cab?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=387&q=80' alt=''/>
+const Imagerender=()=>{
 
+   
+    return (
+        <div>
+            <div className='imageren' > 
+                <img src='https://images.unsplash.com/photo-1669617450318-318cc22b9cab?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=387&q=80' alt=''/>
+                <img src='https://images.unsplash.com/photo-1669617450318-318cc22b9cab?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=387&q=80' alt=''/>
+    
+            </div>
+          
         </div>
-      
-    </>
-  )
+      )
+   
+ 
 }
 
 export default Imagerender
--- image-serch/src/index.js
+++ image-serch/src/index.js
@@ -3,7 +3,8 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 // import App from './App';
 // import Imagerender from './components/Imagerender';
-import reportWebVitals from './reportWebVitals';
+// import * as serviceWorker from "./components/serviceWorker"
+// import reportWebVitals from './reportWebVitals';
 import Search from './components/Search';
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
@@ -17,4 +18,3 @@ root.render(
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
